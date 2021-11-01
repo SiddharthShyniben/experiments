b0368db7f9e8ac3af2c2fc8b39eb035c5cde8cc3
Siddharth <siddharth.muscat@gmail.com>
2021-11-01 09:06:58 +0530

    Add builder

---
+++ css-api/build.ts
@@ -0,0 +1,16 @@
+const contents = Deno.readTextFileSync('./template.css');
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.toLocaleString('default', {month: 'long'}).toLowerCase();
+const day = date.toLocaleString('default', {weekday: 'long'}).toLowerCase();
+const dayCount = date.toLocaleString('default', {day: 'numeric'}).toLowerCase();
+const leap = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ? '--leap: ;' : '';
+
+const out = contents.replace('%{year}', year.toString())
+	.replace('%{month}', month)
+	.replace('%{day}', day)
+	.replace('%{day-count}', dayCount)
+	.replace('%{leap}', leap);
+
+Deno.writeTextFileSync('gen.css', out);
